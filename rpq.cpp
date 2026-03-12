cb5c864a65daaf9a68f0f8524cc005686271ad86
Kuba <kubaszac089@gmail.com>
2026-03-12 19:16:52 +0100

    sorting by R, own simple sorting method, calculating max time

--- rpq.cpp
+++ rpq.cpp
@@ -1,10 +1,3 @@
-// wczytywanie zadan z txt
-// wektor o rozmiarze 24 z wartosciami z pliku
-// sprawdzanie czy dwa razy nie robimy tego samego zadania
-// funkcja wyznaczanie wartosc czasu rozwiazania zadan dla ustalonej kolejnosci(narazie 1, 2, 3, ...)
-// dzisiaj sortowanie po R
-
-// pozniej sprobowac rozwiazac to po swojemu(zamiast sort RQ)
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -12,50 +5,57 @@
 
 using namespace std;
 
-int max_time(int size, const vector<vector<int> > data) {
-    int time = 0;
-    int max_time = 0;
+class rpq{
+    public:
+        rpq() {}
+        ~rpq() {}
 
-    for(int i = 0; i < size; i++) {
-        time = max(time, data[i][0]) + data[i][1];
-        max_time = (max(time+data[i][2],max_time));
-    }
-    
-    return max_time;
-}
+        int max_time(int size, const vector<vector<int> >& data) {
+            int time = 0;
+            int max_time = 0;
 
-vector<vector<int> > sort_r(vector<vector<int> > data) {
-    sort(data.begin(), data.end());
-    return data;
-}
-
-int max_x(int n, const vector<vector<int> > data) {
-    int max = data[0][n];
-    for(int i = 1; i < data.size(); i++) {
-        if(data[i][1] > max) {
-            max = data[i][1];
+            for(int i = 0; i < size; i++) {
+                time = max(time, data[i][0]) + data[i][1];
+                max_time = (max(time+data[i][2],max_time));
+            }
+            
+            return max_time;
         }
-    }
-    return max;
-}
 
-// wlasna metoda sortowania
-vector<vector<int> > my_sort(int size, vector<vector<int> > data) {
-    int max[3] = {max_x(0, data)/size, max_x(1, data)/size, max_x(2, data)/size};
-    int t = 0;
+        vector<vector<int> > sort_r(vector<vector<int> >& data) {
+            sort(data.begin(), data.end());
+            return data;
+        }
+        // wlasna metoda uproszczonego sortowania
+        vector<vector<int> > my_sort(int size, vector<vector<int> >& data) {
+            int max[3] = {max_x(0, data)/size, max_x(1, data)/size, max_x(2, data)/size};
+            int t = 0;
 
-    while(t < 3) {
-        for(int j = 0; j < 3; j++) {
-            for(int i = 0; i < size-1; i++) {
-                if(data[i+1][j] - data[i][j] > max[j]) {
-                    swap(data[i], data[i+1]);
+            while(t < size/12) {
+                for(int j = 0; j < 3; j++) {
+                    for(int i = 0; i < size-1; i++) {
+                        if(data[i+1][j] - data[i][j] > max[j]) {
+                            swap(data[i], data[i+1]);
+                        }
+                    } 
                 }
-            } 
+                t++;
+            }
+            return data;
         }
-        t++;
-    }
-    return data;
-}
+
+    private:
+        int max_x(int n, const vector<vector<int> >& data) {
+            int max = data[0][n];
+            for(int i = 1; i < data.size(); i++) {
+                if(data[i][n] > max) {
+                    max = data[i][n];
+                }
+            }
+            return max;
+        }
+
+    };
 
 int main() {
     ifstream file("data1.txt");
@@ -63,11 +63,11 @@ int main() {
     int size = 0;
     vector<vector<int> > data;
 
-    while (file.is_open()) {
-        if(size == 0) {
-            getline(file, line);
-            size = stoi(line);
-        }
+    if (file.is_open()) {
+
+        getline(file, line);
+        size = stoi(line);
+
         while (getline(file, line)) {
             vector<int> row;
             stringstream s_line(line);
@@ -76,21 +76,25 @@ int main() {
             while (getline(s_line, number, ' ')) {
                 row.push_back(stoi(number));
             }
+
             data.push_back(row);
         }
+
         file.close();
-    } 
-    //          dol prawo
-    
-    int time = max_time(size, data);
-    cout << "max time = " << time<< endl;
+    }
 
-    vector<vector<int> > sorted = sort_r(data);
-    int time2 = max_time(size, sorted);
-    cout << "max time sorted = " << time2<< endl;
+    rpq solver;
 
-    vector<vector<int> > my_sorted = my_sort(size, data);
-    int time3 = max_time(size, my_sorted);
-    cout << "max time my method sorted = " << time3<< endl;
+    int time = solver.max_time(size, data);
+    cout << "max time = " << time << endl;
+
+    vector<vector<int> > sorted = solver.sort_r(data);
+    int time2 = solver.max_time(size, sorted);
+    cout << "max time sorted = " << time2 << endl;
+
+    vector<vector<int> > my_sorted = solver.my_sort(size, data);
+    int time3 = solver.max_time(size, my_sorted);
+    cout << "max time my method sorted = " << time3 << endl;
+
     return 0;
 }
